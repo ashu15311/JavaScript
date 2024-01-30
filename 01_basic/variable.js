4bdeef989876ba581990482d7d35e0c02e2146e0
Ashutosh Sahoo <128279260+ashu15311@users.noreply.github.com>
2024-01-30 12:37:55 +0000

    Variable

---
+++ 01_basic/variable.js
@@ -0,0 +1,12 @@
+var a = 24;
+let b= 30;
+const k=100;
+
+console.log([a,b,k])
+
+const accountId= 122333;
+let accountEmail="ashu@gmail.com";
+var accountPassword="herono1";
+
+// accountId="14444";
+console.log([accountId,accountEmail,accountPassword]);
