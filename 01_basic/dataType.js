d9a5e5a50845f9915c657daa31861c32e81892d2
Ashutosh Sahoo <128279260+ashu15311@users.noreply.github.com>
2024-02-01 12:24:41 +0000

    dataType

---
+++ 01_basic/dataType.js
@@ -0,0 +1,39 @@
+// // Numbers:
+// let length = 16;
+// let weight = 7.5;
+// console.log([typeof(length),typeof(weight)]);
+
+// // Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
+// console.log([typeof(color),typeof(lastName)]);
+
+// // Booleans
+// let x = true;
+// let y = false;
+// console.log([typeof(x),typeof(y)]);
+
+// // BigInt value
+// const value1 = 900719925124740998n;
+// console.log(typeof value1);
+
+// // undefined
+// let nam;
+// console.log(typeof(nam));
+
+// // two symbols with the same description
+// const value1 = Symbol('hello');
+// const value2 = Symbol('hello');
+// console.log(value1==value2);
+
+// //object
+// const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+// console.log(typeof(person));
+
+// //undefined
+// let car;
+// console.log(typeof(car));
+
+// //The typeof(array) is object
+// const cars = ["Saab", "Volvo", "BMW"];
+// console.log(typeof(cars));
