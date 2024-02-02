448b6a0d6de9db0c75fd8a6acfa035fad0905e32
Ashutosh Sahoo <128279260+ashu15311@users.noreply.github.com>
2024-02-02 17:31:28 +0000

    CH 1

---
+++ Ch1/PRACTICE/practice.js
@@ -0,0 +1,31 @@
+//Q1
+let str="Rahul";
+str= str+ "1";
+console.log(str);
+
+// Q2
+console.log(typeof str);
+
+//Q3
+const student={
+        name:"Abhi",
+        "branch":"ETC",
+        "roll":"1"
+}
+//student=54;
+console.log(student);
+// student.roll='3';
+// console.log(student);
+
+//Q4
+student.mark='100';
+console.log(student);
+
+//Q5
+const dict={
+        apple:"seu",
+        copy:"khata",
+        rice:"bhata",
+        potato:"alu"
+}
+console.log(dict);
---
+++ Ch1/datatype.js
@@ -0,0 +1,47 @@
+//strings example
+const name = 'ram';
+const name1 = "hari";
+const result = `The names are ${name} and ${name1}`;
+console.log(typeof name);
+
+//number
+const number1 = 3;
+const number2 = 3.433;
+const number3 = 3e5 // 3 * 10^5
+console.log(typeof number1);
+
+// BigInt value
+const value1 = 900719925124740998n;
+console.log(typeof value1);
+// Adding two big integers
+const result1 = value1 + 1n;
+console.log(result1); // "900719925124740999n"
+
+//Boolean
+const dataChecked = true;
+console.log(typeof dataChecked);
+
+//undefined
+let book;
+console.log(typeof book); // undefined
+
+//null
+let age = null;
+console.log(typeof age);  //object
+
+//symbol
+let symbol1 = Symbol("Geeks")
+let symbol2 = Symbol("Geeks") 
+// Each time Symbol() method 
+// is used to create new global Symbol
+console.log(symbol1 == symbol2); // False
+
+
+//object
+const student = {
+    firstName: 'ram',
+    lastName: null,
+    class: 10
+};
+console.log(student);
+
