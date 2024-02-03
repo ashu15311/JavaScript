91ca6017132117596b7f91221a803fb4d1d2036e
Ashutosh Sahoo <128279260+ashu15311@users.noreply.github.com>
2024-02-03 17:44:00 +0000

    Ch 3 practice

---
+++ Ch3/PRACTICE/practice.js
@@ -0,0 +1,33 @@
+// ***************Q1****************************
+// let obj={
+//     harry:"98",
+//     rohan:"70",
+//     akash:"7"
+// }
+// +++++++++++++++Q2+++++++++++++++++++++++++++++
+// for
+
+// ********************Q3*************************
+// for(let key in obj){
+//     console.log(key,obj[key]);
+// }
+// ++++++++++++++++++++Q4++++++++++++++++++++++++
+let cn=9;
+let i;
+while(i!=cn){
+    i=prompt("Enter number:");
+    if(i!=cn){
+        console.log("Guess correct number !");
+    }
+    else{
+        console.log("Success !");
+    }
+}
+
+// ---------------------Q5------------------------
+// function mean(a,b,c,d){
+//     return ((a+b+c+d)/4);
+// }
+
+// let ans=mean(5,5,5,5);
+// console.log("The mean is:"+ans);
