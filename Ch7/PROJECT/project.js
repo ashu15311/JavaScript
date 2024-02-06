9693b68b8ae33ba74fb77c895bf0fac890a1d78a
Ashutosh Sahoo <128279260+ashu15311@users.noreply.github.com>
2024-02-06 13:40:47 +0000

    Chapter 7

---
+++ Ch7/PROJECT/project.js
@@ -0,0 +1,24 @@
+//   ++++++++++++++   Q1   +++++++++++++++++++++
+
+// const p1=document.querySelector("li");
+// // console.log(p1);
+// p1.style.color="red";
+
+//   +++++++++++++++++  Q3  ++++++++++++++++++++++
+
+// let change1=document.body.children[0].firstElementChild
+// let change2=document.body.children[0].lastElementChild
+// change1.style.color="green";
+// change2.style.color="green";
+
+//  -----------------  Q4  ----------------------
+
+// let a=document.querySelectorAll("li")
+// a.forEach(element => {
+//     element.style.backgroundColor="red";
+// });
+
+//  ++++++++++++++++        Q5   ++++++++++++++++
+
+
+
---
+++ Ch7/script.js
@@ -0,0 +1,38 @@
+console.log("hello");
+
+//    +++++++++++++++++++++   ClassName   ++++++++++++++++++++++++++
+
+// const boox=document.getElementsByClassName("box");
+// boox[0].style.backgroundColor="red";
+
+
+//    +++++++++++++++++++++++++  idName   +++++++++++++++++++++++++++
+
+// const boox=document.getElementById("box4");
+// boox.style.backgroundColor="red";
+
+
+//    -------------------------   tagName   ---------------------------
+
+// const boox=document.getElementsByTagName("div");
+// console.log(boox);
+// boox[0].style.backgroundColor="yellow";
+// boox[1].style.backgroundColor="violet";
+// boox[2].style.backgroundColor="lightgreen";
+// boox[3].style.backgroundColor="cyan";
+// boox[4].style.backgroundColor="orange";
+
+
+//    +++++++++++++++++++++++  QuerySelector  +++++++++++++++++++++++++++
+
+// let boox=document.querySelector("div");
+// boox.style.backgroundColor="green";
+
+//  +++++++++++++++++++++++  QuerySelectorAll  ++++++++++++++++++++++++++
+
+// let qs=document.querySelectorAll("div")
+// console.log(qs);
+
+
+//   >>>>>>>>>>>>>>>>>>>>>>>>  Matches  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
